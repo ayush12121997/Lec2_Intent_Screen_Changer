07c92519b05fdb0d3ff5e8b181f6be0732d5cf8d
Ayush Shrivastava <ayush16024@iiitd.ac.in>
2019-08-12 17:34:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ayush/lec2intentscreenchanger/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ayush.lec2intentscreenchanger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    public void goToNext(View view)
+    {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
